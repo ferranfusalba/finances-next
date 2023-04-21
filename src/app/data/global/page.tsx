2c4dfbdc14ba753b5acdaeebdc2ade3fa3b364fa
Ferran Fusalba Roselló <52327422+ferranfusalba@users.noreply.github.com>
2023-04-21 20:12:10 +0200

    Add links

--- src/app/data/global/page.tsx
+++ src/app/data/global/page.tsx
@@ -1,3 +1,9 @@
+import Link from "next/link"
+
 export default function DataGlobal() {
-    return <div>Global</div>
+    return <div>
+        Global
+        <br />
+        <Link href="/data">Back to Data</Link>
+        </div>
 }
