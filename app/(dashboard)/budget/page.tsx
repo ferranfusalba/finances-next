12fbbd94be2716c6ee719ecb9075a39c3f16ac0a
Ferran Fusalba Roselló <52327422+ferranfusalba@users.noreply.github.com>
2023-06-19 19:38:30 +0200

    Add budget, data, settings pages

---
+++ app/(dashboard)/budget/page.tsx
@@ -0,0 +1,8 @@
+export default function Budget() {
+  return (
+    <div className="layout-submenu">
+      <h1>Budget</h1>
+      <p>Budget Menu</p>
+    </div>
+  );
+}
---
+++ app/(dashboard)/settings/page.tsx
@@ -0,0 +1,8 @@
+export default function Settings() {
+  return (
+    <div className="layout-submenu">
+      <h1>Settings</h1>
+      <p>Settings Menu</p>
+    </div>
+  );
+}
