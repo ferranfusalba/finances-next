5c93a3766c382b78101ff42fe97472256761bbdb
Ferran Fusalba Roselló <ferranfusalba@gmail.com>
2023-10-15 19:18:11 +0200

    Enhance TabBar

--- src/components/TabBar/TabBar.tsx
+++ src/components/TabBar/TabBar.tsx
@@ -1,18 +1,49 @@
 import Link from "next/link";
 
+import {
+  Home,
+  TableBuilt,
+  Currency,
+  ChartLineData,
+  Settings,
+} from "@carbon/icons-react";
+
+type Section = {
+  path: string;
+  name: string;
+  icon: React.ReactNode;
+};
+
+const routes: Array<Section> = [
+  { path: "/", name: "Home", icon: <Home /> },
+  {
+    path: "/accounts",
+    name: "Accounts",
+    icon: <Currency />,
+  },
+  { path: "/budget", name: "Budget", icon: <TableBuilt /> },
+  { path: "/data", name: "Data", icon: <ChartLineData /> },
+  { path: "/settings", name: "Settings", icon: <Settings /> },
+];
+
+function TabBarSection({ section }: { section: Section }) {
+  return (
+    <Link href={section.path}>
+      <li className="flex content-center gap-2 pr-6">
+        <div className="self-center">{section.icon}</div>
+        {section.name}
+      </li>
+    </Link>
+  );
+}
+
 export default function TabBar() {
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link href="/">Home</Link>
-        </li>
-        <li>
-          <Link href="/about">About</Link>
-        </li>
-        <li>
-          <Link href="/accounts">Accounts</Link>
-        </li>
+    <nav className="bg-slate-900 px-6 py-3 text-white">
+      <ul className="flex justify-start">
+        {routes.map((route) => {
+          return <TabBarSection key={route.path} section={route} />;
+        })}
       </ul>
     </nav>
   );
