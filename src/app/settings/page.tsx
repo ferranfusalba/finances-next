15aff317e365b0f346e551cb7c8200f5fdd9105a
Ferran Fusalba Roselló <ferranfusalba@gmail.com>
2023-10-19 18:32:04 +0200

    Create settings page

---
+++ src/app/settings/page.tsx
@@ -0,0 +1,9 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Settings | Finances Next",
+};
+
+export default function SettingsPage() {
+  return <h1>Settings</h1>;
+}
