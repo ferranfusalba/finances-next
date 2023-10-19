76803e64666bbbd00a7d734b11517d84552f549b
Ferran Fusalba Roselló <52327422+ferranfusalba@users.noreply.github.com>
2023-10-19 20:08:39 +0200

    Delete about page

--- src/app/about/page.tsx
+++
@@ -1,9 +0,0 @@
-import type { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: "About | Finances Next",
-};
-
-export default function AboutPage() {
-  return <h1>About</h1>;
-}
