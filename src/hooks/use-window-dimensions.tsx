3eecb30bffe5d3f960860c7b42d8b726ef44909a
Ferran Fusalba Roselló <52327422+ferranfusalba@users.noreply.github.com>
2023-10-21 19:47:11 +0200

    Add window dimensions hook for ssr

---
+++ src/hooks/use-window-dimensions.tsx
@@ -0,0 +1,33 @@
+"use client";
+import { useEffect, useState } from "react";
+
+type WindowDimentions = {
+  width: number | string;
+  height: number | string;
+};
+
+const useWindowDimensions = (): WindowDimentions => {
+  const [windowDimensions, setWindowDimensions] = useState<WindowDimentions>({
+    width: "",
+    height: "",
+  });
+
+  useEffect(() => {
+    function handleResize(): void {
+      setWindowDimensions({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    }
+
+    handleResize();
+
+    window.addEventListener("resize", handleResize);
+
+    return (): void => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return windowDimensions;
+};
+
+export default useWindowDimensions;
