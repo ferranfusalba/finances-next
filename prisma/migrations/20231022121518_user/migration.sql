149269abdd7d0a7b96fbd3c4a7ac827607a985be
Ferran Fusalba Roselló <ferranfusalba@gmail.com>
2023-10-22 18:41:18 +0200

    Add user to schema & update local urls

---
+++ prisma/migrations/20231022121518_user/migration.sql
@@ -0,0 +1,26 @@
+-- AlterTable
+ALTER TABLE "Account" ADD COLUMN     "userId" INTEGER;
+
+-- AlterTable
+ALTER TABLE "Budget" ADD COLUMN     "userId" INTEGER;
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "Account" ADD CONSTRAINT "Account_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Budget" ADD CONSTRAINT "Budget_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
