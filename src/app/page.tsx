6cc2c714a03b52e2247d91739cc9fdccd51e9205
Ferran Fusalba Roselló <52327422+ferranfusalba@users.noreply.github.com>
2023-10-22 20:53:46 +0200

    Add session & user info on homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,39 @@
-export default function Home() {
+import ClientSession from "@/components/home/ClientSession";
+import { authConfig } from "@/libs/auth";
+import { User } from "@/types/User";
+import getUserId from "@/utils/getUserId";
+import { getServerSession } from "next-auth";
+
+export default async function Home() {
+  const serverSession = await getServerSession(authConfig);
+
+  const user = await getUserId(serverSession?.user?.email as string);
+
+  if (serverSession) {
+    return (
+      <>
+        <h1>Finances Home</h1>
+        <h3>Session info (server):</h3>
+        <p>serverSession.user.name: {serverSession?.user?.name}</p>
+        <p>serverSession.user.email: {serverSession?.user?.email}</p>
+        <p>
+          serverSession.user.image:{" "}
+          <img
+            src={serverSession?.user?.image as string}
+            alt="Profile picture"
+          />
+        </p>
+        <ClientSession />
+        <h3>User info</h3>
+        <p>user.id: {user?.id}</p>
+        <p>user.name: {user?.name}</p>
+        <p>user.email: {user?.email}</p>
+        <p>user.password: {user?.password}</p>
+        <p>user.createdAt: {user?.createdAt?.toString()}</p>
+        <p>user.updatedAt: {user?.updatedAt?.toString()}</p>
+      </>
+    );
+  }
+
   return <h1>(Finances Home)</h1>;
 }
---
+++ src/components/home/ClientSession.tsx
@@ -0,0 +1,19 @@
+"use client";
+import { useSession } from "next-auth/react";
+
+export default function ClientSession() {
+  const { data: session } = useSession();
+
+  return (
+    <>
+      <h3>Session info (client):</h3>
+      <p>session.expires: {session?.expires}</p>
+      <p>session.user.name: {session?.user?.name}</p>
+      <p>session.user.email: {session?.user?.email}</p>
+      <p>
+        session.user.image:{" "}
+        <img src={session?.user?.image as string} alt="Profile picture" />
+      </p>
+    </>
+  );
+}
