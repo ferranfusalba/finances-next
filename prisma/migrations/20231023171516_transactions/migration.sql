7b983ba1278fb9cd0f0f375a34e12ac1bd949d47
Ferran Fusalba Roselló <ferranfusalba@gmail.com>
2023-10-23 19:15:26 +0200

    Add transaction model

---
+++ prisma/migrations/20231023171516_transactions/migration.sql
@@ -0,0 +1,21 @@
+-- CreateTable
+CREATE TABLE "Transaction" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "concept" TEXT NOT NULL,
+    "type" TEXT NOT NULL,
+    "import" TEXT NOT NULL,
+    "currency" TEXT NOT NULL,
+    "notes" TEXT NOT NULL,
+    "accountId" INTEGER,
+    "budgetId" INTEGER,
+
+    CONSTRAINT "Transaction_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Transaction" ADD CONSTRAINT "Transaction_accountId_fkey" FOREIGN KEY ("accountId") REFERENCES "Account"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Transaction" ADD CONSTRAINT "Transaction_budgetId_fkey" FOREIGN KEY ("budgetId") REFERENCES "Budget"("id") ON DELETE SET NULL ON UPDATE CASCADE;
