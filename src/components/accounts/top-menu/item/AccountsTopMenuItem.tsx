759fc2dc3f0e913aceaddc06edd028c1c47b6cf4
Ferran Fusalba Roselló <52327422+ferranfusalba@users.noreply.github.com>
2023-10-23 19:11:11 +0200

    Fix accounts & budgets top menu items width

--- src/components/accounts/top-menu/item/AccountsTopMenuItem.tsx
+++ src/components/accounts/top-menu/item/AccountsTopMenuItem.tsx
@@ -6,7 +6,7 @@ export default function AccountMenuItem({ account }: { account: Account }) {
   return (
     <li
       key={account.id}
-      className="border-2 border-white flex justify-center items-center min-w-10rem mx-2.5"
+      className="border-2 border-white flex justify-center items-center min-w-10rem mx-2.5 px-4"
     >
       <Link href={"/accounts/" + account.id}>{account.name}</Link>{" "}
     </li>
--- src/components/budgets/top-menu/item/BudgetsTopMenuItem.tsx
+++ src/components/budgets/top-menu/item/BudgetsTopMenuItem.tsx
@@ -6,7 +6,7 @@ export default function BudgettMenuItem({ budget }: { budget: Budget }) {
   return (
     <li
       key={budget.id}
-      className="border-2 border-white flex justify-center items-center min-w-10rem mx-2.5"
+      className="border-2 border-white flex justify-center items-center min-w-10rem mx-2.5 px-4"
     >
       <Link href={"/budgets/" + budget.id}>{budget.name}</Link>{" "}
     </li>
