74d1fd08256339e7e530f2c8e08409388e7a4fef
Ferran Fusalba Roselló <ferranfusalba@gmail.com>
2024-01-05 18:22:28 +0100

    Fix accounts & budgets top menu items style

--- src/components/accounts/top-menu/item/AccountsTopMenuItem.tsx
+++ src/components/accounts/top-menu/item/AccountsTopMenuItem.tsx
@@ -6,9 +6,14 @@ export default function AccountMenuItem({ account }: { account: Account }) {
   return (
     <li
       key={account.id}
-      className="border-2 border-white flex justify-center items-center min-w-10rem mx-2.5 px-4"
+      className="border-2 border-white flex justify-center items-center mx-2.5 px-4"
     >
-      <Link href={"/accounts/" + account.id}>{account.name}</Link>{" "}
+      <Link
+        className="w-max min-w-10rem text-center"
+        href={"/accounts/" + account.id}
+      >
+        {account.name}
+      </Link>{" "}
     </li>
   );
 }
