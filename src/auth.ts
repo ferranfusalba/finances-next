f8f491cc38bb5b2d9763e6dcaf88e2183ab4f3fe
Ferran Fusalba Roselló <52327422+ferranfusalba@users.noreply.github.com>
2024-01-12 18:22:24 +0100

    Complete section - resend (sending emails)

--- src/auth.ts
+++ src/auth.ts
@@ -12,14 +12,17 @@ export const {
   signOut,
 } = NextAuth({
   callbacks: {
-    // async signIn({ user }) {
-    //   const existingUser = await getUserById(user.id);
+    async signIn({ user, account }) {
+      // Allow OAuth without email verification
+      if (account?.provider !== "credentials") return true;
 
-    //   if (!existingUser || !existingUser.emailVerified) {
-    //     return false;
-    //   }
-    //   return true;
-    // },
+      const existingUser = await getUserById(user.id);
+
+      // Prevent sign in without email verification
+      if (!existingUser?.emailVerified) return false;
+
+      return true;
+    },
     async session({ token, session }) {
       if (token.sub && session.user) {
         session.user.id = token.sub;
