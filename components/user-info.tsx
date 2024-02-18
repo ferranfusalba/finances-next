dcc8ca5ee6eaba371aa196db866d68824d951122
Ferran <52327422+ferranfusalba@users.noreply.github.com>
2024-02-18 20:30:34 +0100

    Fix color user info

--- components/user-info.tsx
+++ components/user-info.tsx
@@ -15,31 +15,31 @@ export const UserInfo = ({ user, label }: UserInfoProps) => {
       <CardContent className="space-y-4">
         <div className="flex flex-row items-center justify-between rounded-lg border p-3 shadow-sm">
           <p className="text-sm font-md">ID</p>
-          <p className="truncate text-xs max-w-[180px] font-mono p-1 bg-slate-100 rounded-md">
+          <p className="truncate text-xs max-w-[180px] font-mono p-1 bg-slate-100 rounded-md text-stone-900">
             {user?.id}
           </p>
         </div>
         <div className="flex flex-row items-center justify-between rounded-lg border p-3 shadow-sm">
           <p className="text-sm font-md">Name</p>
-          <p className="truncate text-xs max-w-[180px] font-mono p-1 bg-slate-100 rounded-md">
+          <p className="truncate text-xs max-w-[180px] font-mono p-1 bg-slate-100 rounded-md text-stone-900">
             {user?.name}
           </p>
         </div>
         <div className="flex flex-row items-center justify-between rounded-lg border p-3 shadow-sm">
           <p className="text-sm font-md">Email</p>
-          <p className="truncate text-xs max-w-[180px] font-mono p-1 bg-slate-100 rounded-md">
+          <p className="truncate text-xs max-w-[180px] font-mono p-1 bg-slate-100 rounded-md text-stone-900">
             {user?.email}
           </p>
         </div>
         <div className="flex flex-row items-center justify-between rounded-lg border p-3 shadow-sm">
           <p className="text-sm font-md">Role</p>
-          <p className="truncate text-xs max-w-[180px] font-mono p-1 bg-slate-100 rounded-md">
+          <p className="truncate text-xs max-w-[180px] font-mono p-1 bg-slate-100 rounded-md text-stone-900">
             {user?.role}
           </p>
         </div>
         <div className="flex flex-row items-center justify-between rounded-lg border p-3 shadow-sm">
           <p className="text-sm font-md">2FA</p>
-          <p className="truncate text-xs max-w-[180px] font-mono p-1 bg-slate-100 rounded-md">
+          <p className="truncate text-xs max-w-[180px] font-mono p-1 bg-slate-100 rounded-md text-stone-900">
             N/A
           </p>
         </div>
