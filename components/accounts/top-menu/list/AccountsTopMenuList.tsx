322975946a40ac1241a30aff50aa80bd3b810e6d
Ferran <ferranfusalba@gmail.com>
2024-03-03 17:02:44 +0100

    Add selected account logic & styles on accounts top menu

--- components/accounts/top-menu/list/AccountsTopMenuList.tsx
+++ components/accounts/top-menu/list/AccountsTopMenuList.tsx
@@ -1,6 +1,5 @@
-"use client";
 import { Account } from "@/types/Account";
-import Link from "next/link";
+import AccountsTopMenuListItem from "./AccoutnsTopMenuListItem";
 
 export default function AccountsTopMenuList({
   accountList,
@@ -11,17 +10,10 @@ export default function AccountsTopMenuList({
     <>
       <ol className="flex overflow-auto flex-nowrap scroll-touch">
         {accountList.map((account, index) => (
-          <li
-            key={index}
-            className="border-2 border-white flex justify-center items-center mx-2.5 px-4"
-          >
-            <Link
-              className="w-max min-w-10rem text-center"
-              href={"/accounts/" + account.id}
-            >
-              {account.name}
-            </Link>{" "}
-          </li>
+          <AccountsTopMenuListItem
+            account={account}
+            index={index}
+          ></AccountsTopMenuListItem>
         ))}
       </ol>
     </>
