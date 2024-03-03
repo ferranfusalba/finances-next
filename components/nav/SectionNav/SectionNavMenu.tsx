c09670c224bb4f1a83feb28248e021d5f8b129a1
Ferran <52327422+ferranfusalba@users.noreply.github.com>
2024-03-03 17:31:29 +0100

    Add selected logic & styles to add account button

--- components/nav/SectionNav/SectionNavMenu.tsx
+++ components/nav/SectionNav/SectionNavMenu.tsx
@@ -1,9 +1,8 @@
-import Link from "next/link";
 import { db } from "@/lib/db";
-import { AddAlt } from "@carbon/icons-react";
 import { auth } from "@/auth";
 import getUserId from "@/utils/getUserId";
 import SectionNavMenuList from "./list/SectionNavMenuList";
+import SectionNavMenuAdd from "./SectionNavMenuAdd";
 
 async function loadUserAccounts(userId: string) {
   return await db.financialAccount.findMany({
@@ -23,11 +22,7 @@ export default async function SectionNavMenu() {
     <>
       <nav className="flex bg-sky-900 fixed top-16 w-full z-10">
         <ul>
-          <Link href="/accounts/new">
-            <li className="border-2 border-white flex justify-center items-center mr-2.5 w-11 h-11">
-              <AddAlt />
-            </li>
-          </Link>
+          <SectionNavMenuAdd />
         </ul>
         <SectionNavMenuList accountList={userAccounts} />
       </nav>
---
+++ components/nav/SectionNav/SectionNavMenuAdd.tsx
@@ -0,0 +1,25 @@
+"use client";
+import Link from "next/link";
+import { AddAlt } from "@carbon/icons-react";
+import { usePathname } from "next/navigation";
+import { cn } from "@/lib/utils";
+
+export default async function SectionNavMenuAdd() {
+  const pathname = usePathname();
+  const newAccountSelected = pathname === "/accounts/new";
+
+  return (
+    <>
+      <Link href="/accounts/new">
+        <li
+          className={cn(
+            "border-2 border-white flex justify-center items-center mr-2.5 w-11 h-11",
+            { "bg-neutral-50 text-sky-900": newAccountSelected }
+          )}
+        >
+          <AddAlt />
+        </li>
+      </Link>
+    </>
+  );
+}
