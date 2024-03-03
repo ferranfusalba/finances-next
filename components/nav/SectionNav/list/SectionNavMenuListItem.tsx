ae3518c0ffdc0f0b9b31cb6c81433254a685b70d
Ferran <ferranfusalba@gmail.com>
2024-03-03 17:25:35 +0100

    Update component function names

--- components/nav/SectionNav/list/SectionNavMenuListItem.tsx
+++ components/nav/SectionNav/list/SectionNavMenuListItem.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { cn } from "@/lib/utils";
 
-export default function AccountsTopMenuListItem({
+export default function SectionNavMenuListItem({
   account,
   index,
 }: {
