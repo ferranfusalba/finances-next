155dfd4739b0436cc79928296c0e4b01e195b528
Ferran <52327422+ferranfusalba@users.noreply.github.com>
2024-03-03 18:35:15 +0100

    Apply section nav to budgets

--- app/accounts/layout.tsx
+++ app/accounts/layout.tsx
@@ -1,18 +1,34 @@
 import SectionNavMenu from "@/components/nav/SectionNav/SectionNavMenu";
 import type { Metadata } from "next";
+import { db } from "@/lib/db";
+import { auth } from "@/auth";
+import getUserId from "@/utils/getUserId";
 
 export const metadata: Metadata = {
   title: "Accounts | Finances Next",
 };
+
+async function loadUserAccounts(userId: string) {
+  return await db.financialAccount.findMany({
+    where: {
+      userId: userId,
+    },
+  });
+}
 
 export default async function AccountsLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const session = await auth();
+  const userEmail = session?.user?.email as string;
+  const userId = await getUserId(userEmail);
+  const userAccounts = await loadUserAccounts(userId?.id as string);
+
   return (
     <>
-      <SectionNavMenu />
+      <SectionNavMenu type="accounts" list={userAccounts} />
       {/* TODO: Solve this TS error */}
       <main className="h-full-main-mobile md:h-full-main pt-11">
         {children}
