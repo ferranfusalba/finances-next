3a366585d2acbfb022b7e645f9a0464f75992837
Ferran <ferranfusalba@gmail.com>
2024-03-03 17:07:08 +0100

    Add iteration key

--- components/accounts/top-menu/list/AccountsTopMenuList.tsx
+++ components/accounts/top-menu/list/AccountsTopMenuList.tsx
@@ -13,6 +13,7 @@ export default function AccountsTopMenuList({
           <AccountsTopMenuListItem
             account={account}
             index={index}
+            key={account.id}
           ></AccountsTopMenuListItem>
         ))}
       </ol>
