4b29a7550efa64449db5bb8a24120df5eb0743e9
Ferran <ferranfusalba@gmail.com>
2024-03-09 16:28:11 +0100

    Rename account table & transaction

--- app/api/budgets/[id]/route.ts
+++ app/api/budgets/[id]/route.ts
@@ -1,8 +1,8 @@
 import { db } from "@/lib/db";
-import { BudgetParamsProps } from "@/types/Budget";
+import { AccountBudgetParamsProps } from "@/types/AccountBudget";
 import { NextResponse } from "next/server";
 
-export async function GET(request: any, { params }: BudgetParamsProps) {
+export async function GET(request: any, { params }: AccountBudgetParamsProps) {
   const budget = await db.budget.findUnique({
     where: {
       id: params.id,
@@ -12,7 +12,7 @@ export async function GET(request: any, { params }: BudgetParamsProps) {
   return NextResponse.json(budget);
 }
 
-export async function PUT(request: any, { params }: BudgetParamsProps) {
+export async function PUT(request: any, { params }: AccountBudgetParamsProps) {
   const data = await request.json();
   await db.budget.update({
     where: {
@@ -24,7 +24,10 @@ export async function PUT(request: any, { params }: BudgetParamsProps) {
   return NextResponse.json("Updating Budget " + params.id);
 }
 
-export async function DELETE(request: any, { params }: BudgetParamsProps) {
+export async function DELETE(
+  request: any,
+  { params }: AccountBudgetParamsProps
+) {
   try {
     const budgetDeleted = await db.budget.delete({
       where: {
