922ccbfc4ebf4b01fbfbdd9500dff165400c464a
Ferran Fusalba Roselló <ferranfusalba@gmail.com>
2024-06-19 10:12:44 +0200

    Fix section & home active handling

--- components/nav/BottomNav/BottomNavSection.tsx
+++ components/nav/BottomNav/BottomNavSection.tsx
@@ -10,14 +10,16 @@ import { Section } from "@/components/nav/BottomNav/BottomNav";
 export default function BottomNavSection({ section }: { section: Section }) {
   const pathname = usePathname();
 
-  const isActive = section.path === pathname;
+  const isActive =
+    section.name !== "Home" ? pathname.includes(section.path) : null;
+  const isHomeActive = pathname === section.path;
 
   return (
     <Link href={section.path}>
       <li
         className={cn(
           "flex items-center justify-center gap-2 md:px-6 flex-col md:flex-row h-full",
-          { "bg-white text-stone-900": isActive }
+          { "bg-white text-stone-900": isActive || isHomeActive }
         )}
       >
         <div className="self-center">{section.icon}</div>
