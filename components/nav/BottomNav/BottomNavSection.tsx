e9ad4e785e3c15d0c8440aa51f5021a3004cede9
Ferran Fusalba Roselló <52327422+ferranfusalba@users.noreply.github.com>
2024-06-19 09:54:17 +0200

    Enhance bottom nav sections by adding isActive bg handling

--- components/nav/BottomNav/BottomNavSection.tsx
+++ components/nav/BottomNav/BottomNavSection.tsx
@@ -1,11 +1,25 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+import { cn } from "@/lib/utils";
 
 import { Section } from "@/components/nav/BottomNav/BottomNav";
 
 export default function BottomNavSection({ section }: { section: Section }) {
+  const pathname = usePathname();
+
+  const isActive = section.path === pathname;
+
   return (
     <Link href={section.path}>
-      <li className="flex content-center gap-2 md:pr-6 flex-col md:flex-row">
+      <li
+        className={cn(
+          "flex items-center justify-center gap-2 md:px-6 flex-col md:flex-row h-full",
+          { "bg-white text-stone-900": isActive }
+        )}
+      >
         <div className="self-center">{section.icon}</div>
         <span className="text-xs md:text-base">{section.name}</span>
       </li>
