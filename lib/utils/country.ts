790ac6125d2776cfd1f90fa907898acf752d8b4a
Ferran Fusalba Roselló <ferranfusalba@gmail.com>
2024-06-22 13:47:56 +0200

    Move get colors & currency functions to specific utils, enhance Transfer to Destination Account conditionals

---
+++ lib/utils/country.ts
@@ -0,0 +1,13 @@
+import countries_code from "@/statics/countries_code.json";
+
+const countriesCode: {
+  [k: string]: {
+    name: string;
+    "alpha-2": string;
+    "emoji-flag": string;
+  };
+} = countries_code;
+
+export const getCountryFlag = (alpha2Code: string) => {
+  return countriesCode[alpha2Code]["emoji-flag"];
+};
