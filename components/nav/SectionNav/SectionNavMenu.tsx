2abb29626b1c5efcd67204850f968583702bbc8e
Ferran Fusalba Roselló <52327422+ferranfusalba@users.noreply.github.com>
2025-03-15 18:18:05 +0100

    Enhance account button when inactive, sort account table from time origin

--- components/nav/SectionNav/SectionNavMenu.tsx
+++ components/nav/SectionNav/SectionNavMenu.tsx
@@ -10,7 +10,7 @@ export default async function SectionNavMenu({
 }: {
   type: string;
   allowAdd?: boolean;
-  list: Array<{ id: string; name: string }>;
+  list: Array<{ id: string; name: string; active?: boolean }>;
 }) {
   return (
     <>
