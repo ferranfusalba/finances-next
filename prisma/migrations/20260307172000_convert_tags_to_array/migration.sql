d10da6d4c4d2abcae885df58e32e2e58c0c36eb5
Ferran Fusalba Roselló <52327422+ferranfusalba@users.noreply.github.com>
2026-03-07 17:26:10 +0100

    Revert "Add missing Prisma migrations for tags array and preferencesSet"
    This reverts commit 622b3338f16aba431c13941f59950dce0b69314f.

--- prisma/migrations/20260307172000_convert_tags_to_array/migration.sql
+++
@@ -1,6 +0,0 @@
--- AlterTable: convert tags from nullable text to text array
-ALTER TABLE "AccountTransaction" DROP COLUMN "tags",
-ADD COLUMN "tags" TEXT[] DEFAULT ARRAY[]::TEXT[];
-
-ALTER TABLE "BudgetTransaction" DROP COLUMN "tags",
-ADD COLUMN "tags" TEXT[] DEFAULT ARRAY[]::TEXT[];
--- prisma/migrations/20260307172100_add_preferences_set/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "User" ADD COLUMN "preferencesSet" BOOLEAN NOT NULL DEFAULT false;
